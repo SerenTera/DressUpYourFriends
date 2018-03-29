604d2489f1882cee2a99f9fe8f3abbfe641dcfb7
Seren <cmingxuan94@gmail.com>
2018-03-30 01:31:22 +0800

    v3.0.0: Fixes for male brawler patch, auto update

--- modes.js
+++ modes.js
@@ -1,39 +1,30 @@
 //This file contains all the potential mode combinations.
 //modefunc contains the array index of customequipname that is to be excluded from being copied over.
-const customequipname = {
+
 	
-//This list all the possible costume combinations	
-	0:'weapon',  		//General equips on 1st tab in character profile
-	1:'body',
-	2:'hand',
-	3:'feet',
-	4:'underwear',
-	5:'head',
-	6:'face',
-	7:'weaponModel',	//I have to freaking idea whats model for but whatever
-	8:'bodyModel',
-	9:'handModel',
-	10:'feetModel',
-	11:'weaponDye',		//Dyes for general equips.
-	12:'bodyDye',		
-	13:'handDye',
-	14:'feetDye',
-	15:'underwearDye',
-	16:'styleBackDye',
-	17:'styleHeadDye',
-	18:'styleFaceDye',
-	19:'weaponEnchant',	//Weapon Enchantment. 0-9 in this current patch I think.
-	20:'styleHead',		//The proper costumes on second tab
-	21:'styleFace',
-	22:'styleBack',
-	23:'styleWeapon',
-	24:'styleBody',
-	25:'styleFootprint',
-	26:'styleBodyDye'	//Dye for Body costume. Only applies if the costume is dyeable
-}
+//This list all the possible costume combinations, in order
+//General equips on 1st tab in character profile
+//weaponModel,etc : I have to freaking idea whats model for but whatever
+//Dye: Dyes for general equips.
+//weaponEnchant: Weapon Enchantment. 0-9 in this current patch I think.
+//styleHead, etc etc: The proper costumes on second tab
+//acessory 
+//styleBodyDye: Dye for Body costume. Only applies if the costume is dyeable
 		  
 //List of modes:
-//indexes to keep follow customequipname, just add modes by using this format (modenumber)=[index,index,index....], eg 7:[1,2] to keep weapon and chest.
+//REFER TO the list above or in fieldId in costumePacket.json for adding modes 
+//Indexes to keep follow fieldId, just add modes by using this format (modenumber)=[index,index,index....], eg 7:[1,2] to keep weapon and chest.
+
+const path = require('path'),
+	  fs = require('fs')
+
+try {
+	customequipname = JSON.parse(fs.readFileSync(path.join(__dirname,'lib','costumePacket.json'), 'utf8')).fieldId
+}
+catch(e){customequipname={}}  //Temporary
+
+
+
 const modefunc={
 			0:[], 		 		//[] (DEFAULT: NO MAINTAINING OF TARGET COSTUME)
 			1:[0,7,23],  		//keeps targets weapon
@@ -42,10 +33,16 @@ const modefunc={
 			4:[20,21,22,24], 	//keep costume, including mask, back, hairadornment.
 			5:[1,12],			//keeps target body 
 			6:[0,7]				//keep target weapon w/o skin
-		};
+}
 		
 module.exports = {		
-
+	
+	update() {
+		customequipname = JSON.parse(fs.readFileSync(path.join(__dirname,'lib','costumePacket.json'), 'utf8')).fieldId
+		console.log('[DressUpFriends] Updated modes.js with new version defs. Refer to fieldId in costumePacket.json')
+		return
+	},
+	
 	totalmode() {
 		return Object.keys(modefunc).length;
 	},
@@ -73,28 +70,7 @@ module.exports = {
 			}
 		}
 		return newequipset
-	},
+	}
 	
-	/*changesingle(stringindex,customid) { //single string changer
-		let singleequip={};
-		if(stringindex.length===customid.length) {
-			for(let i in stringindex) {
-				singleequip[customequipname[stringindex[i]]]=customid[i];
-			}
-		}
-		return singleequip
-	}*/
-	
-	/*function message(msg) {
-		dispatch.toClient('S_CHAT', 1, {
-			channel: 24,
-			authorID: 0,
-			unk1: 0,
-			gm: 0,
-			unk2: 0,
-			authorName: '',
-			message: '(DressUpFriends)'+msg
-		});
-	};*/
-};		
+}
 		
