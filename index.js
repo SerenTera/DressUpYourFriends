9b77818dafd9e1bc446a74c563167909c14a736c
Seren <cmingxuan94@gmail.com>
2018-08-22 16:56:17 +0800

    Refactor for require command deprecation

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 //Configuration file is config.json (NOT configDefault.json)
 
 const modes=require('./modes'),
-	  Command = require('command'),
 	  path = require('path'),
 	  fs = require('fs'),	  
 	  defaultConfig = require('./lib/configDefault.json')
@@ -10,8 +9,7 @@ const modes=require('./modes'),
 const CHANGER_ABNORMALITY=[7777001,7777003,7777007,7000005,7000001,7000011,806001], //Current Abnormality ids of shape changers/self confidence/zombie pot
 	  customchat=['weapon','chest','inner','chestdye','enchantment','hat','mask','back','weaponskin','costume','costumedye'] //shortcut name for custom changes: [weapon,chest,innerwear,chestDye,weaponEnchant,hairAdornment,mask,back,weaponSkin,costume,costumeDye]
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-module.exports = function dressupf(dispatch) {
-	const command = Command(dispatch)
+module.exports = function dressupf(mod) {
 	
 	let players={},
 		changed=[],
@@ -80,214 +78,221 @@ module.exports = function dressupf(dispatch) {
 	
 	
 /////Commands
-	command.add('dressup',namestr => {
-		namestr=namestr.toLowerCase()
-		if(namestr === datanamestring) {
-			dispatch.toClient('S_USER_EXTERNAL_CHANGE', 6, equips)
-			command.message('(Dressup)You Changed </3')
-		}
-		if(players[namestr]) {
-			pushcostume(namestr,players[namestr].id,players[namestr].playerequip)
-			command.message('(Dressup)Changed '+namestr+' (Mode: '+JSON.stringify(modemessage)+')')
-		}
-	})
+	mod.command.add('du', {
+		
+		change(namestr) {
+			namestr=namestr.toLowerCase()
+			if(namestr === datanamestring) {
+				mod.send('S_USER_EXTERNAL_CHANGE', 6, equips)
+				mod.command.message('(Dressup)You Changed </3')
+			}
+			if(players[namestr]) {
+				pushcostume(namestr,players[namestr].id,players[namestr].playerequip)
+				mod.command.message('(Dressup)Changed '+namestr+' (Mode: '+JSON.stringify(modemessage)+')')
+			}
+		},
+		
+		$default() {
+			mod.command.message('(Dressup) Wrong Command.')
+		},
+		save(namestring) {
+			namestring=namestring.toLowerCase()
+			if(players[namestring]) {
+				playersave=true
+				pushcostume(namestring,players[namestring].id,players[namestring].playerequip)
+				playersave=false
+				saveplayer(customdata,'playerdata.json')
+				mod.command.message('(Dressup)Changed and saved '+namestring+' (Mode: '+JSON.stringify(modemessage)+')')
+			}
+		},
 	
-	command.add('dressupsave',namestring => {
-		namestring=namestring.toLowerCase()
-		if(players[namestring]) {
-			playersave=true
-			pushcostume(namestring,players[namestring].id,players[namestring].playerequip)
-			playersave=false
-			saveplayer(customdata,'playerdata.json')
-			command.message('(Dressup)Changed and saved '+namestring+' (Mode: '+JSON.stringify(modemessage)+')')
-		}
-	})
-	
-	command.add('dressupdelete',deletename => {
-		deletename=deletename.toLocaleLowerCase()
-		if(customdata[deletename]) {
+		delete(deletename) {
+			deletename=deletename.toLocaleLowerCase()
+			if(customdata[deletename]) {
 				delete customdata[deletename]
 				saveplayer(customdata,'playerdata.json')
-				command.message('(Dressup)Deleted '+deletename+' from saved data')
-		}
-	})
+				mod.command.message('(Dressup)Deleted '+deletename+' from saved data')
+			}
+		},
 	
 	//Commands that affects features when applying the equipments
-	command.add('duchangers', () => {
-		negateChangers = !negateChangers
-		command.message(`(Dressup)Negate shape changers ${negateChangers ? 'enabled' : 'disabled'}`)
-	})
+		changers() {
+			negateChangers = !negateChangers
+			mod.command.message(`(Dressup)Negate shape changers ${negateChangers ? 'enabled' : 'disabled'}`)
+		},
 	
-	command.add('dufake', () => {
-		ignoreFake = !ignoreFake
-		command.message(`(Dressup)Ignore fake equipment sent by other modules ${ignoreFake ? 'enabled' : 'disabled'}`);
-	})
+		fake() {
+			ignoreFake = !ignoreFake
+			mod.command.message(`(Dressup)Ignore fake equipment sent by other modules ${ignoreFake ? 'enabled' : 'disabled'}`);
+		},
 	
-	command.add('dumode', arg => {
-		mode=parseInt((arg.replace(/[^0-9\.]/g, '')),10)
-		if(isNaN(mode)) {
-			let singlemode=arg.replace(/ /g, '').toLowerCase().split(",")
-			mode=[]
-			modemessage=[]
-			for(let name of packetname) {
-				if(singlemode.includes(name.toLowerCase())) {
-					mode.push(name)
-					modemessage.push(name)
-				}	
+		mode(arg) {
+			mode=parseInt((arg.replace(/[^0-9\.]/g, '')),10)
+			if(isNaN(mode)) {
+				let singlemode=arg.replace(/ /g, '').toLowerCase().split(",")
+				mode=[]
+				modemessage=[]
+				for(let name of packetname) {
+					if(singlemode.includes(name.toLowerCase())) {
+						mode.push(name)
+						modemessage.push(name)
+					}					
+				}
+				mod.command.message('(Dressup)Mode changed. Following parts will be kept: '+ modemessage)
 			}
-			command.message('(Dressup)Mode changed. Following parts will be kept: '+ modemessage)
-		}
-		else if(mode>(modes.totalmode()-1)) {
-			mode=0
-			modemessage=0
-			command.message('(Dressup)Invalid mode, resetting to default mode=0')
-		}
-		else
-			modemessage=[mode],
-			command.message('(Dressup)Mode changed to '+mode)
-	})
+			else if(mode>(modes.totalmode()-1)) {
+				mode=0
+				modemessage=0
+				mod.command.message('(Dressup)Invalid mode, resetting to default mode=0')
+			}
+			else
+				modemessage=[mode],
+				mod.command.message('(Dressup)Mode changed to '+mode)
+		},
 	
-	command.add('duid', namestri => {
-		namestri=namestri.toLowerCase()
-		if(players[namestri]) {
-			let idtext={}
-			playeridequip = players[namestri].playerequip
-			for(let i in packetname) idtext[packetname[i]] = playeridequip[i]
-			command.message('(Dressup)Player '+namestri+' costume ids are:\n'+ JSON.stringify(idtext, null, "\t"))
-			console.log('(Dressup)Player '+namestri+' costume ids are:\n'+ JSON.stringify(idtext, null, "\t"))
-		}
-	})
+		id(namestri) {
+			namestri=namestri.toLowerCase()
+			if(players[namestri]) {
+				let idtext={}
+				playeridequip = players[namestri].playerequip
+				for(let i in packetname) idtext[packetname[i]] = playeridequip[i]
+				mod.command.message('(Dressup)Player '+namestri+' costume ids are:\n'+ JSON.stringify(idtext, null, "\t"))
+				console.log('(Dressup)Player '+namestri+' costume ids are:\n'+ JSON.stringify(idtext, null, "\t"))
+			}
+		},
 	
 	//Customise saved equipment
-	command.add('ducustom', (argu,name) => {
-		if(argu.includes('json')) {
-			fs.readFile(path.join(__dirname,'ducustom.json'),function(err,data) {
-				if(err) {
-					command.message('(Dressup) Error reading file ducustom.json. Remember to blank out entries if not used as ""')
+		custom: {
+			json(name) {
+				fs.readFile(path.join(__dirname,'ducustom.json'),function(err,data) {
+					if(err) {
+						mod.command.message('(Dressup) Error reading file ducustom.json. Remember to blank out entries if not used as ""')
+						return
+					}
+					else
+						data=JSON.parse(data)
+						Object.entries(data).forEach(([key,values]) => {
+							if(isNaN(parseInt(values))) delete data[key]
+							else 
+								data[key] = parseInt(values)
+						})
+						Object.assign(equips,data)
+						mod.command.message('(Dressup)Sucessfully modified saved equip. Changes: '+JSON.stringify(data))
+				})
+			},
+			fix(name) {//does not work maybe
+				if(!CUSTOM_MOD) return
+				mod.command.message('(Dressup)Custom fix mode in effect, the next costume change will not be applied but will be saved');
+				mod.hookOnce('S_USER_EXTERNAL_CHANGE',6,{order:7,filter:{fake:true}}, event => { //Only if you use custom mods that changes equip via S_USER_EXTERNAL_CHANGE
+					if(event.gameId.equals(playerid)) {	
+						equips = Object.assign({},event)
+						mod.command.message('(Dressup)New equip saved, not applied to player')
+						return false
+					}
+				})
+			},
+			$default(name) {
+				customstring=argu.replace(/[0-9\"\'\:\{\}]/g, '').replace(/ /g,'').toLowerCase().split(",") //Sorry, i went fuck it and left it ugly :/
+				customidstr=argu.replace(/[^0-9\,]/g, '').replace(/ /g,'').split(",")
+				stringindex=[]
+				customid=[]
+				if(customstring.length !== customidstr.length) {
+					mod.command.message('(Dressup)Custom part change error: Mismatch id and part. Check spellings and , is used to separate parts.')
 					return
 				}
+			
+				for(let name of packetname) {
+					if(customstring.includes(name.toLowerCase())) {
+						newcustom[name] = customidstr[customstring.indexOf(name.toLowerCase())]
+					}
+				}
+		
+				if(typeof(newcustom)=== 'object'){Object.assign(equips,newcustom)}  //repeatable and will continue to be kept so long as it is not changed again.
+			
+				if(Object.keys(newcustom).length===0) {
+					mod.command.message('(Dressup)Custom part change error: Empty/Wrong input. Check spellings and , is used to separate parts.')
+				}
 				else
-					data=JSON.parse(data)
-					Object.entries(data).forEach(([key,values]) => {
-						if(isNaN(parseInt(values))) delete data[key]
-						else 
-							data[key] = parseInt(values)
-					})
-					Object.assign(equips,data)
-					command.message('(Dressup)Sucessfully modified saved equip. Changes: '+JSON.stringify(data))
-			})
-		}
-		else if(argu.includes('fix') && CUSTOM_MOD) { //does not work maybe
-			command.message('(Dressup)Custom fix mode in effect, the next costume change will not be applied but will be saved');
-			dispatch.hookOnce('S_USER_EXTERNAL_CHANGE',6,{order:7,filter:{fake:true}}, event => { //Only if you use custom mods that changes equip via S_USER_EXTERNAL_CHANGE
-				if(event.gameId.equals(playerid)) {	
-					equips = Object.assign({},event)
-					command.message('(Dressup)New equip saved, not applied to player')
-					return false
-				}
-			})
-		}
-		else {
-			customstring=argu.replace(/[0-9\"\'\:\{\}]/g, '').replace(/ /g,'').toLowerCase().split(",") //Sorry, i went fuck it and left it ugly :/
-			customidstr=argu.replace(/[^0-9\,]/g, '').replace(/ /g,'').split(",")
-			stringindex=[]
-			customid=[]
-			if(customstring.length !== customidstr.length) {
-				command.message('(Dressup)Custom part change error: Mismatch id and part. Check spellings and , is used to separate parts.')
-				return
-			}
-			
-			for(let name of packetname) {
-				if(customstring.includes(name.toLowerCase())) {
-					newcustom[name] = customidstr[customstring.indexOf(name.toLowerCase())]
-				}
+					mod.command.message('(Dressup)Custom part change '+ JSON.stringify(newcustom))
 			}
 		
-			if(typeof(newcustom)=== 'object'){Object.assign(equips,newcustom)}  //repeatable and will continue to be kept so long as it is not changed again.
-			
-			if(Object.keys(newcustom).length===0) {
-				command.message('(Dressup)Custom part change error: Empty/Wrong input. Check spellings and , is used to separate parts.')
-			}
-			else
-				command.message('(Dressup)Custom part change '+ JSON.stringify(newcustom));
-		}
 		
-		
-	})
+		},
 	
 	//Import/export data
-	command.add('duimport',namearg => {
-		fs.readFile(path.join(__dirname,'importdata.json'),function(err,data) {
-			if(err) {
-				command.message('(Dressup) Error reading file importdata.json. Check that it is in module folder')
-				return
-			}
-			data=JSON.parse(data)
-			if(typeof namearg === 'undefined') {
-				Object.assign(customdata,data)
-				saveplayer(customdata,'playerdata.json')
-				command.message('(Dressup) Attempted to Import all data')
-			}
-			else if(data[namearg.toLowerCase()]) {
-				customdata[namearg.toLowerCase()]=data[namearg.toLowerCase()]
-				saveplayer(customdata,'playerdata.json')
-				command.message('(Dressup) Attempted to Import data for '+ namearg)
-			}
-			else
-				command.message('(Dressup) Error Importing file: Name not found')
-		})
-	})
+		import(namearg) {
+			fs.readFile(path.join(__dirname,'importdata.json'),function(err,data) {
+				if(err) {
+					mod.command.message('(Dressup) Error reading file importdata.json. Check that it is in module folder')
+					return
+				}
+				data=JSON.parse(data)
+				if(typeof namearg === 'undefined') {
+					Object.assign(customdata,data)
+					saveplayer(customdata,'playerdata.json')
+					mod.command.message('(Dressup) Attempted to Import all data')
+				}
+				else if(data[namearg.toLowerCase()]) {
+					customdata[namearg.toLowerCase()]=data[namearg.toLowerCase()]
+					saveplayer(customdata,'playerdata.json')
+					mod.command.message('(Dressup) Attempted to Import data for '+ namearg)
+				}
+				else
+					mod.command.message('(Dressup) Error Importing file: Name not found')
+			})
+		},
 
-	command.add('duexport',() => {
-		importdata[datanamestring] = makesavefile(originalequips)
-		saveplayer(importdata,'importdata.json')
-		command.message('(Dressup) Created import file. Send importdata.json to another player to have them fix how all your exported characters look like on their end.')
-	})
+		export() {
+			importdata[datanamestring] = makesavefile(originalequips)
+			saveplayer(importdata,'importdata.json')
+			mod.command.message('(Dressup) Created import file. Send importdata.json to another player to have them fix how all your exported characters look like on their end.')
+		},
 		
 	//Disable/Enable Module
-	command.add('dutoggle',() => {
-		if(enabled) {
-			enabled=false
-			players=[]
-			changed=[]
-			command.message('(Dressup)Disabled DressupFriends')
-		}
-		else {
-			enabled=true,
-			command.message('(Dressup)Enabled DressupFriends')
-		}
-	})
-	
-	command.add('dupacket',() => {
-		command.message('[DressupFriends] Attempting to check for vital packet changes. Unequip something')
-		packetUpdate('*')
-		modes.update()
-	})
-	
-	command.add('dumigrate',() => {
-		let tempObj = {}
-		Object.keys(customdata).forEach( key => {
-			if(!customdata[key].showStyle) {
-				tempObj = customdata[key].changers
-				delete customdata[key].changers
-				customdata[key].showStyle = true
-				customdata[key].changers = tempObj
+		toggle() {
+			if(enabled) {
+				enabled=false
+				players=[]
+				changed=[]
+				mod.command.message('(Dressup)Disabled DressupFriends')
 			}
-		})
-		saveplayer(customdata,'playerdata.json')
-		command.message('[DressupFriends] Sucessfully migrated playerdata.json to new format')
+			else {
+				enabled=true,
+				mod.command.message('(Dressup)Enabled DressupFriends')
+			}
+		},
+	
+		packet() {
+			mod.command.message('[DressupFriends] Attempting to check for vital packet changes. Unequip something')
+			packetUpdate('*')
+			modes.update()
+		},
+	
+		migrate() {
+			let tempObj = {}
+			Object.keys(customdata).forEach( key => {
+				if(!customdata[key].showStyle) {
+					tempObj = customdata[key].changers
+					delete customdata[key].changers
+					customdata[key].showStyle = true
+					customdata[key].changers = tempObj
+				}
+			})
+			saveplayer(customdata,'playerdata.json')
+			mod.command.message('[DressupFriends] Sucessfully migrated playerdata.json to new format')
+		}
 	})
 ////////Dispatches
-	dispatch.hook('S_LOGIN', 10, event => {
+	mod.hook('S_LOGIN', 10, event => {
 		playerid = event.gameId
 		datanamestring=event.name.toLowerCase()
-		if(costumePacket.gameVersion != dispatch.base.protocolVersion) {
+		if(costumePacket.gameVersion != mod.majorPatchVersion) {
 			console.log('[DressupFriends] New game version detected. Attempt to check for vital packet changes upon login.')
 			packetUpdate('*')
 		}
 	})
 
-	dispatch.hook('S_SPAWN_USER',13,event => {
+	mod.hook('S_SPAWN_USER',13,event => {
 		if(enabled)	{
 			playername = event.name.toLowerCase()
 			
@@ -305,19 +310,19 @@ module.exports = function dressupf(dispatch) {
 				
 				Object.assign(event,customdata[playername])
 				delete event.changers
-				if(MESSAGE_OVERRIDE_CHANGES) command.message(`Changed ${event.name} to saved costume`)
+				if(MESSAGE_OVERRIDE_CHANGES) mod.command.message(`Changed ${event.name} to saved costume`)
 				
 				return true
 			}
 		}
 	})
 	
-	dispatch.hook('S_ABNORMALITY_BEGIN', 2, event => {
+	mod.hook('S_ABNORMALITY_BEGIN', 2, event => {
 		if(changerBlock[event.target] && CHANGER_ABNORMALITY.includes(event.id)) return false
 	})
 	
 	
-	dispatch.hook('S_DESPAWN_USER', 3, event => { 	
+	mod.hook('S_DESPAWN_USER', 3, event => { 	
 		for(let [key, value] of Object.entries(players)) {
 			if(event.gameId.equals(value.id)) {
 				delete players[key]
@@ -327,7 +332,7 @@ module.exports = function dressupf(dispatch) {
 		}
 	})
 	
-	dispatch.hook('S_LOAD_TOPO', 'raw', () => { 
+	mod.hook('S_LOAD_TOPO', 'raw', () => { 
 		players=[]
 		changed=[]
 		changerBlock=[]
@@ -335,13 +340,13 @@ module.exports = function dressupf(dispatch) {
 
 	
 
-	dispatch.hook('S_USER_EXTERNAL_CHANGE', 6, {order:6,filter:{fake:null}}, (event,fake) => { 	//hooks for fake packets from other modules, hook later to prevent clashes?
+	mod.hook('S_USER_EXTERNAL_CHANGE', 6, {order:6,filter:{fake:null}}, (event,fake) => { 	//hooks for fake packets from other modules, hook later to prevent clashes?
 		if(ignoreFake && fake) return
 		
 		if(enabled && event.gameId.equals(playerid)) {  //NOT A SPELLING ERROR ~.~
 			equips = event	
 			originalequips = event
-			command.message('(Dressup) Current Equipped saved')
+			mod.command.message('(Dressup) Current Equipped saved')
 		}
 		
 		else if(MAINTAIN_COSTUME && changed.length!==0 && !fake) { 
@@ -351,20 +356,20 @@ module.exports = function dressupf(dispatch) {
 	
 /////Functions
 	function packetUpdate(version) {
-		dispatch.hookOnce('S_USER_EXTERNAL_CHANGE' ,version , {order:-100,filter:{fake:null}}, event => {
+		mod.hookOnce('S_USER_EXTERNAL_CHANGE' ,version , {order:-100,filter:{fake:null}}, event => {
 			let compareName = Object.keys(event)
 			let matchId = {}
 			compareName.shift() //Remove 1st gameId
 			
 			if(arraysEqual(packetname,compareName)) {
 				console.log('[DressupFriends] No new packet changes detected in new game revision.')
-				saveplayer(Object.assign({},costumePacket,{gameVersion:dispatch.base.protocolVersion}),['lib','costumePacket.json'])
+				saveplayer(Object.assign({},costumePacket,{gameVersion:mod.majorPatchVersion}),['lib','costumePacket.json'])
 			}
 				
 			else {
 				packetname = compareName
 				console.log('[DressupFriends] Detected changes in packet definition. Update module packet [S_USER_EXTERNAL_CHANGE]')
-				costumePacket.gameVersion = dispatch.base.protocolVersion
+				costumePacket.gameVersion = mod.majorPatchVersion
 				for(i=0; i < packetname.length; i++) {
 					matchId[i] = packetname[i]
 				}
@@ -372,7 +377,7 @@ module.exports = function dressupf(dispatch) {
 				
 				saveplayer({
 					"packetField":packetname,
-					"gameVersion":dispatch.base.protocolVersion,
+					"gameVersion":mod.majorPatchVersion,
 					"fieldId": matchId
 				},['lib','costumePacket.json'])
 				
@@ -391,7 +396,7 @@ module.exports = function dressupf(dispatch) {
 	
 	function endabnormality(target) {
 		for(let skillid of CHANGER_ABNORMALITY) {
-			dispatch.toClient('S_ABNORMALITY_END',1, {
+			mod.send('S_ABNORMALITY_END',1, {
 				target: target,
 				id: skillid
 			})
@@ -403,7 +408,7 @@ module.exports = function dressupf(dispatch) {
 		newcostume = Object.assign({},equips,{gameId:playeridn},targetmodeeqp)
 		if(negateChangers) endabnormality(playeridn)
 			
-		dispatch.toClient('S_USER_EXTERNAL_CHANGE', 6, newcostume)
+		mod.send('S_USER_EXTERNAL_CHANGE', 6, newcostume)
 		
 		if(playersave) customdata[playerign] = makesavefile(newcostume)
 		
@@ -424,7 +429,7 @@ module.exports = function dressupf(dispatch) {
 		if(fileopen) {
 			fileopen=false
 			fs.writeFile(path.join(__dirname, ...args), JSON.stringify(data,null,"\t"), err => {
-				if(err) command.message('Error Writing File, attempting to rewrite')
+				if(err) mod.command.message('Error Writing File, attempting to rewrite')
 				fileopen = true
 				if(!err && data.gameVersion) modes.update()
 			})
